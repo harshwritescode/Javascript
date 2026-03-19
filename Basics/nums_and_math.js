b77c4056f893008a976ac9f2889a438d37e5269e
Harsh Dixit <dixitharsh248@gmail.com>
2026-03-19 15:42:53 +0530

    Mathematics done

---
+++ Basics/nums_and_math.js
@@ -0,0 +1,47 @@
+
+/* --------------------------------------------------- NUMS -----------------------------------------------------------*/
+
+const score = 200;
+const balance = new Number(100.678);
+/*
+a)console.table([score, balance.valueOf()]);
+
+┌─────────┬────────┐
+│ (index) │ Values │
+├─────────┼────────┤
+│ 0       │ 200    │
+│ 1       │ 1000   │
+└─────────┴────────┘
+b)
+console.log(score);
+console.log(balance);
+
+200
+[Number: 1000]
+
+c)
+console.log(balance.toString().length);  //7
+console.log(balance.toFixed(2));  //100.68
+console.log(balance.toPrecision(4));  //100.7
+
+d)
+const hundreds =1000000;
+console.log(hundreds.toLocaleString('en-In'));   //10,00,000
+*/
+
+
+
+
+
+/*       ----------------------------------  Maths --------------------------------------------------------------   */
+
+// console.log(Math);
+// console.log(Math.abs(-9));
+// console.log(Math.round(3.3));
+// console.log(Math.ceil(4.3));   //top
+// console.log(Math.floor(3.3)); //least
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,8));
+
+
+console.log(Math.random()); // lies between in 0 to 1

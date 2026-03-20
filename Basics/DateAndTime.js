df15c2b32083fd3efc233f555895efcc4babd8af
Harsh Dixit <dixitharsh248@gmail.com>
2026-03-20 17:02:21 +0530

    DateandTime Knowledge

--- Basics/DateAndTime.js
+++ Basics/DateAndTime.js
@@ -1,2 +1,17 @@
-let TodayDate=new Date();
-console.log(TodayDate);
+/*
+1)let TodayDate=new Date();
+
+console.log(TodayDate); //2026-03-19T10:18:42.954Z
+console.log(TodayDate.getDate());    //19
+console.log(TodayDate.toString());  //Thu Mar 19 2026 15:49:44 GMT+0530 (India Standard Time)
+console.log(TodayDate.toDateString());  //Thu Mar 19 2026
+
+2)
+let customdate=new Date(2023,0,23);
+console.log(customdate.toDateString()); //Mon Jan 23 2023 it means months is starting with 0 not 1
+let timestamp=Date.now();
+console.log(Math.floor(timestamp/1000)) //second 1773916116
+
+*/
+
+

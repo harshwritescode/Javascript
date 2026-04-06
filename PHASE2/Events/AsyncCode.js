3b73c5b9805499f59b4e05145b1c222911aba775
Harsh Dixit <dixitharsh248@gmail.com>
2026-04-06 22:01:15 +0530

    Basic Javascript

---
+++ PHASE2/Events/AsyncCode.js
@@ -0,0 +1,14 @@
+/*
+  Javascript
+      a) Synchronous
+      b) Single Threaded      (Default)
+   Execution Context
+      Execution one line of code at a time
+      console.log(1)                  
+      console.log(2)
+                          
+                         Each operation waits for the last one to complete before executing
+
+      call Stack         Memory Heap
+
+ */

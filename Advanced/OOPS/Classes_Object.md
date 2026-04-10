163d912130b33c303ece185666cd0c6b75973581
Harsh Dixit <dixitharsh248@gmail.com>
2026-04-10 16:18:40 +0530

    Updated the folder structure and introduce the oops concept

---
+++ Advanced/OOPS/Classes_Object.md
@@ -0,0 +1,58 @@
+  /* Do javascript really have classes??
+  
+  Yes, JavaScript does have classes, introduced in ES6, 
+  which provide a clean and structured way to create objects and handle inheritance. 
+  However, unlike traditional class-based languages 
+  such as Java or C++, JavaScript is fundamentally prototype-based, 
+  meaning these classes are not true classes under the hood. Instead, 
+  they act as syntactic sugar over JavaScript’s existing prototype system, 
+  making the code easier to read and write while still relying on prototypes for actual behavior.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------------
+
+
+  📌 JavaScript and Classes
+
+🔹 OOP (Object-Oriented Programming)
+JavaScript supports OOP concepts like objects, classes (syntactic sugar), inheritance, etc.
+
+🔹 Object
+An object is a collection of properties (data) and methods (functions)
+
+Example:
+{
+  name: "Harsh",        // property
+  age: 22,              // property
+  greet: function() {   // method
+    console.log("Hello");
+  }
+}
+
+🔹 toLowerCase()
+A built-in string method in JavaScript
+It converts all characters of a string into lowercase
+
+Example:
+let str = "HELLO";
+console.log(str.toLowerCase()); // "hello"
+
+
+Parts of OOP
+
+a)Object Literals
+b)constructor function
+c)prototypes
+d)classes
+e)Instances (new,this)
+
+ 4 Pillars---
+ Abstraction - Data hide fetch()
+ Encapsulation - wrap up the data
+ Inheritance -- Prototypes
+ Polymorphism -- Same method have same form
+
+  */
+
+
+ 
---
+++ Advanced/OOPS/oop.js
@@ -0,0 +1,57 @@
+
+// --- Object Literals --
+const user ={
+    //Properties
+     username:"Harsh",
+     logincount:8,
+     signedin:true,
+     
+     //methods
+     getuserDetails: function(){
+        console.log("Got User details from database");  
+        // console.log(`Username: ${this.username}`); // Harsh
+        // console.log(this);  
+        /*  Harsh
+            8
+           {
+              username: 'Harsh',
+             logincount: 8,
+             signedin: true,
+             getuserDetails: [Function: getuserDetails]
+           }
+   */
+        
+          
+     }
+    // ---this keyword  always tell us that the real and current context
+
+};
+// console.log(user["username"]);  // Harsh
+// console.log(user.logincount);   // 8
+// // console.log(user.getuserDetails()); // Got User details from database undefined 
+// console.log(this); //global context mein {} this will run because it is node environment
+
+// -----------------------------------------------------------------------------------------------------------------------------------
+
+// Constructor Function  -- new keyword is a constructor function it will create a new instance copy
+// const PromiseOne= new Promise(function (resolve,reject){
+
+// });
+// const date=new Date();
+
+function User(username,logincount,isloogedin){
+    this.username=username;
+    this.logincount=logincount;
+    this.isloogedin=isloogedin;
+
+    return this;
+}
+
+// const UserOne= new User("Harsh",9,true);
+// const UserTwo= new User("Krishna",19,true);  // Override ho jayega
+// console.log(UserOne); // User { username: 'Harsh', logincount: 9, isloogedin: true }
+// console.log(UserTwo); // User { username: 'Krishna', logincount: 19, isloogedin: true }
+
+
+
+
